19a93052c5d4020455442676b4ad30e6db22fbf3
Connor <connormeny@hotmail.com>
2026-04-29 16:24:05 -0700

    Initial Firmware Setup

---
+++ teensy_firmware/UART_Jetson.cpp
@@ -0,0 +1,20 @@
+void UART_Jetson_setup() {
+    Serial.begin(9600);
+    pinMode(13, OUTPUT);
+}
+
+void UART_Jetson_loop() {
+    if (Serial.available()) {
+        String command = Serial.readString();
+        command.trim();
+        if (command == "LED_ON") {
+            digitalWrite(13, HIGH);
+            Serial.println("LED ON");
+        } else if (command == "LED_OFF") {
+            digitalWrite(13, LOW);
+            Serial.println("LED OFF");
+        } else if (command == "PING") {
+            Serial.println("PONG");
+        }
+    }
+}
---
+++ teensy_firmware/teensy_code.ino
@@ -0,0 +1,10 @@
+void main_setup();
+void main_loop();
+
+void setup() {   
+    main_setup();
+}
+
+void loop() {
+    main_loop();
+}
